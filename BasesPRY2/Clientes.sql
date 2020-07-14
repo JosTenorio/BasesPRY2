81ff4c156ccb00c05e6d24dcea6ab105cae5e3b7
TilapiaKIddo2 <jostenoriop@gmail.com>
2020-07-13 19:00:42 -0600

    Solved model questions

---
+++ BasesPRY2/Clientes.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Clientes]
+(
+	[Id] INT NOT NULL IDENTITY,
+	[Nombre] NVARCHAR(50) NOT NULL,
+	[Telefono] NCHAR (8) NOT NULL,
+	CONSTRAINT PkClientes_Id PRIMARY KEY (Id),
+	CONSTRAINT UnClientes_Telefono UNIQUE (Telefono),
+	CONSTRAINT CkClientes_Telefono CHECK (LEN(Telefono) = 8 AND (Telefono NOT LIKE '%[^0-9]%')),
+)
--- BasesPRY2/RegistroPagos.sql
+++ BasesPRY2/RegistroPagos.sql
@@ -4,7 +4,9 @@
     [FechaHoraCompra] DATETIME NOT NULL, 
     [Codigo] NCHAR(6) NOT NULL, 
     [CostoTotal] DECIMAL(18, 2) NOT NULL,
+    [IdCliente] INT NOT NULL,
     CONSTRAINT PkRegistroPagos_Id PRIMARY KEY (Id),
+    CONSTRAINT PkRegistroPagos_IdCliente FOREIGN KEY (IdCliente) REFERENCES Clientes (Id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT CkRegistroPagos_Codigo CHECK ((LEN(Codigo) = 6) AND (CODIGO NOT LIKE '%[^0-9]%')),
     CONSTRAINT CkRegistroPagos_CostoTotal CHECK (CostoTotal > 0)
 )
