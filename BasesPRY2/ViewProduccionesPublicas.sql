d4e260cffae3d8a5a96f1e9da03fdba86195c8f1
TilapiaKIddo2 <jostenoriop@gmail.com>
2020-07-14 15:59:55 -0600

    Added public productions view

---
+++ BasesPRY2/ViewProduccionesPublicas.sql
@@ -0,0 +1,5 @@
+﻿CREATE VIEW [dbo].[ViewProduccionesPublicas]
+	AS SELECT Producciones.Id, Producciones.FechaInicio, Producciones.FechaFin, Producciones.IdTeatro, Producciones.IdObra, Producciones.IdEstado
+	FROM Producciones
+	INNER JOIN Estados ON Producciones.IdEstado = Estados.Id 
+	WHERE Estados.Nombre <> 'Confidencial'
