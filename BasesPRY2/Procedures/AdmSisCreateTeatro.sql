0c008429af9f731a76409797261cc49eba303310
Jose Pablo Munoz <jpmunoz@estudiantec.cr>
2020-07-18 12:57:49 -0600

    Added procedure to create theater

---
+++ BasesPRY2/Procedures/AdmSisCreateTeatro.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[AdmSisCreateTeatro]
+	@Nombre NVARCHAR(50),
+	@Direccion NVARCHAR(100),
+	@Correo NVARCHAR(50),
+	@Link NVARCHAR(50),
+	@TelBoleteria NCHAR(8),
+	@TelAdmin NCHAR(8)
+AS
+	SET NOCOUNT ON
+
+	INSERT INTO Teatros (Nombre, Direccion, Correo, Link, TelBoleteria, TelAdmin)
+	VALUES (@Nombre, @Direccion, @Correo, @Link, @TelBoleteria, @TelAdmin)
+GO
