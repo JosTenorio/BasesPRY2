7dcf3dc34c31a5fb17318f9886c9c91b472a1a9a
Jost <Jost>
2020-07-24 19:57:44 -0600

    Simplified job script

--- Scripts/ActualizacionRoles.sql
+++ Scripts/ActualizacionRoles.sql
@@ -1,11 +1,13 @@
 USE [msdb]
 GO
 
-/****** Object:  Job [ReporteDiario]    Script Date: 24/7/2020 13:46:04 ******/
+/****** Object:  Job [ActualizaciónRoles]    Script Date: 24/7/2020 13:45:43 ******/
 BEGIN TRANSACTION
+DECLARE @LoginName nvarchar (80)
+SET @LoginName = 'JOS-PC\JOS'
 DECLARE @ReturnCode INT
 SELECT @ReturnCode = 0
-/****** Object:  JobCategory [Data Collector]    Script Date: 24/7/2020 13:46:04 ******/
+/****** Object:  JobCategory [Data Collector]    Script Date: 24/7/2020 13:45:43 ******/
 IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'Data Collector' AND category_class=1)
 BEGIN
 EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'Data Collector'
@@ -14,7 +16,7 @@ IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 END
 
 DECLARE @jobId BINARY(16)
-EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'ReporteDiario', 
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'ActualizaciónRoles', 
 		@enabled=1, 
 		@notify_level_eventlog=0, 
 		@notify_level_email=0, 
@@ -23,10 +25,10 @@ EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'ReporteDiario',
 		@delete_level=0, 
 		@description=N'No description available.', 
 		@category_name=N'Data Collector', 
-		@owner_login_name=N'JOS-PC\JOS', @job_id = @jobId OUTPUT
+		@owner_login_name=@LoginName, @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [DailyReport]    Script Date: 24/7/2020 13:46:04 ******/
-EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'DailyReport', 
+/****** Object:  Step [ActulizarRoles]    Script Date: 24/7/2020 13:45:43 ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'ActulizarRoles', 
 		@step_id=1, 
 		@cmdexec_success_code=0, 
 		@on_success_action=1, 
@@ -36,25 +38,25 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'DailyRep
 		@retry_attempts=0, 
 		@retry_interval=0, 
 		@os_run_priority=0, @subsystem=N'TSQL', 
-		@command=N'EXEC SisCreateReporteDiario', 
+		@command=N'EXEC SisUpdateUserRoles', 
 		@database_name=N'BasesPRY2', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Diario a las 5:00', 
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Every15seconds', 
 		@enabled=1, 
 		@freq_type=4, 
 		@freq_interval=1, 
-		@freq_subday_type=1, 
-		@freq_subday_interval=0, 
+		@freq_subday_type=2, 
+		@freq_subday_interval=15, 
 		@freq_relative_interval=0, 
 		@freq_recurrence_factor=0, 
 		@active_start_date=20200724, 
 		@active_end_date=99991231, 
-		@active_start_time=170000, 
+		@active_start_time=0, 
 		@active_end_time=235959, 
-		@schedule_uid=N'8efe9287-246d-4a51-8dc7-56218cca36fa'
+		@schedule_uid=N'd637cd30-4fbc-4266-b415-305fd1e7f178'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
--- Scripts/ReporteDiario.sql
+++ Scripts/ReporteDiario.sql
@@ -1,11 +1,13 @@
 USE [msdb]
 GO
 
-/****** Object:  Job [ActualizaciónRoles]    Script Date: 24/7/2020 13:46:24 ******/
+/****** Object:  Job [ReporteDiario]    Script Date: 24/7/2020 13:46:04 ******/
 BEGIN TRANSACTION
+DECLARE @LoginName nvarchar(100)
+SET @LoginName = 'JOS-PC\JOS'
 DECLARE @ReturnCode INT
 SELECT @ReturnCode = 0
-/****** Object:  JobCategory [Data Collector]    Script Date: 24/7/2020 13:46:24 ******/
+/****** Object:  JobCategory [Data Collector]    Script Date: 24/7/2020 13:46:04 ******/
 IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'Data Collector' AND category_class=1)
 BEGIN
 EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'Data Collector'
@@ -14,7 +16,7 @@ IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 END
 
 DECLARE @jobId BINARY(16)
-EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'ActualizaciónRoles', 
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'ReporteDiario', 
 		@enabled=1, 
 		@notify_level_eventlog=0, 
 		@notify_level_email=0, 
@@ -23,10 +25,10 @@ EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'ActualizaciónRoles',
 		@delete_level=0, 
 		@description=N'No description available.', 
 		@category_name=N'Data Collector', 
-		@owner_login_name=N'Jota-PC\Jota', @job_id = @jobId OUTPUT
+		@owner_login_name=@LoginName, @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [ActulizarRoles]    Script Date: 24/7/2020 13:46:24 ******/
-EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'ActulizarRoles', 
+/****** Object:  Step [DailyReport]    Script Date: 24/7/2020 13:46:04 ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'DailyReport', 
 		@step_id=1, 
 		@cmdexec_success_code=0, 
 		@on_success_action=1, 
@@ -36,25 +38,25 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Actuliza
 		@retry_attempts=0, 
 		@retry_interval=0, 
 		@os_run_priority=0, @subsystem=N'TSQL', 
-		@command=N'EXEC SisUpdateUserRoles', 
+		@command=N'EXEC SisCreateReporteDiario', 
 		@database_name=N'BasesPRY2', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Every15seconds', 
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Diario a las 5:00', 
 		@enabled=1, 
 		@freq_type=4, 
 		@freq_interval=1, 
-		@freq_subday_type=2, 
-		@freq_subday_interval=15, 
+		@freq_subday_type=1, 
+		@freq_subday_interval=0, 
 		@freq_relative_interval=0, 
 		@freq_recurrence_factor=0, 
 		@active_start_date=20200724, 
 		@active_end_date=99991231, 
-		@active_start_time=0, 
+		@active_start_time=170000, 
 		@active_end_time=235959, 
-		@schedule_uid=N'd637cd30-4fbc-4266-b415-305fd1e7f178'
+		@schedule_uid=N'8efe9287-246d-4a51-8dc7-56218cca36fa'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
